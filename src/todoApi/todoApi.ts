f3c312a4d7e9756a8d6fa8a7e4f195b757f0b2f5
Kim CG <cjfrbs1256@gmail.com>
2024-03-15 13:28:30 +0900

    vercel 재배포

--- src/todoApi/todoApi.ts
+++ src/todoApi/todoApi.ts
@@ -1,25 +1,30 @@
 import { Todos } from "@/types/type";
 
 const getTodo = async (): Promise<Todos[]> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_LOCAL_URL}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_LOCAL_URL}/api/todos`
+  );
   const { todos } = await response.json();
   return todos;
 };
 
 const toggleTodo = async ({ id, isDone }: { id: string; isDone: boolean }) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_LOCAL_URL}/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ id, isDone: !isDone }),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_LOCAL_URL}/api/todos/${id}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, isDone: !isDone }),
+    }
+  );
   const toggledTodo = await response.json();
   return toggledTodo;
 };
 
 const deleteTodo = async ({ id }: { id: string }) => {
-  await fetch(`${process.env.NEXT_PUBLIC_LOCAL_URL}/${id}`, {
+  await fetch(`${process.env.NEXT_PUBLIC_LOCAL_URL}/api/todos/${id}`, {
     method: "DELETE",
     body: JSON.stringify({ id }),
   });
